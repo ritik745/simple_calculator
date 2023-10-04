97c3a6ed82b94516cbbc1c438676c14b00cc18a3
ritik745 <146919492+ritik745@users.noreply.github.com>
2023-10-04 11:57:49 +0530

    Add files via upload

---
+++ simplecalculater.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char operation;
+    double num1, num2;
+
+    cout << "Simple Calculator" << endl;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> operation;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    double result;
+
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                cout << "Error: Division by zero" << endl;
+                return 1; 
+            }
+            break;
+        default:
+            cout << "Error: Invalid operator" << endl;
+            return 1; 
+    }
+
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
